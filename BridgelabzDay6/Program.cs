c25122b9813ed41749cc6d7e8513b6c6636a1f9f
Sumeetacz <sumeetacz@gmail.com>
2022-03-18 17:06:12 +0530

    Perfect Number problem

--- BridgelabzDay6/Program.cs
+++ BridgelabzDay6/Program.cs
@@ -22,6 +22,8 @@ namespace LogicalProblems_Day6
                     fibonacci.Fab();
                     break;
                 case 2:
+                    PerfectNumber perfectNumber = new PerfectNumber();
+                    perfectNumber.Pnumber();
                     break;
                 case 3:
                     break;
@@ -63,4 +65,35 @@ class Fibonacci
     }
     
 }
+
+class PerfectNumber
+{
+    public void Pnumber()
+    {
+        int startingNumber, endingNumbeer;
+        Console.WriteLine("Get All Perfect In Range of Between two Number");
+        Console.Write("Enter Starting Number : ");
+        startingNumber = int.Parse(Console.ReadLine());
+        Console.Write("Enter Ending Number : ");
+        endingNumbeer = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Below are the perfect number between " + startingNumber + " and " + endingNumbeer);
+
+        for (int i = startingNumber; i <= endingNumbeer; i++)
+        {
+            decimal sum = 0;
+            for (int j = 1; j < i; j++)
+            {
+                if (i % j == 0)
+                    sum = sum + j;
+            }
+            if (sum == i)
+                Console.WriteLine("\t" + i);
+        }
+
+      
+    }
+  
+}
+
 
