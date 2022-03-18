15772f122b4a47849589707d5fb12c0ade75b1da
Sumeetacz <sumeetacz@gmail.com>
2022-03-18 16:41:06 +0530

    Fibonacci series problem

--- Program.cs
+++ Program.cs
@@ -18,6 +18,8 @@ namespace LogicalProblems_Day6
             switch (Option)
             {
                 case 1:
+                    Fibonacci fibonacci = new Fibonacci();
+                    fibonacci.Fab();
                     break;
                 case 2:
                     break;
@@ -36,4 +38,29 @@ namespace LogicalProblems_Day6
         }
     }
 }
+
+class Fibonacci
+{
+
+    public void Fab()
+    {
+        int n, a = 0, b = 1, c, i;
+        Console.Write("Enter the number of terms : ");
+        n = Int32.Parse(Console.ReadLine());
+        for (i = 0; i < n; i++)
+        {
+            if (i <= 1)
+                c = i;
+            else
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            Console.Write(c + "  ");
+        }
+        Console.ReadKey();
+    }
     
+}
+
